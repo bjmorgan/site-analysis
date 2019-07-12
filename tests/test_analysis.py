fd5bcfb99d59f6be41ffaf0b87471c75e7d198de
Benjamin Morgan <benmorgan2@gmail.com>
2019-07-12 11:57:06 +0100

    Added first unittest

---
+++ tests/test_analysis.py
@@ -0,0 +1,15 @@
+import unittest
+from siteid import Analysis
+
+class AnalysisTestCase(unittest.TestCase):
+
+    def test_analysis_is_initialised(self):
+        atoms = 'foo'
+        polyhedra = 'bar'
+        analysis = Analysis(atoms=atoms, polyhedra=polyhedra)
+        self.assertEqual( analysis.atoms, atoms )
+        self.assertEqual( analysis.polyhedra, polyhedra )
+
+if __name__ == '__main__':
+    unittest.main()
+    

258f7b9f24e43f28fc8dda3eb23bef4e82975f6c
Benjamin Morgan <benmorgan2@gmail.com>
2019-07-12 21:04:31 +0100

    Changes to Atom class

--- site_analysis/atom.py
+++ site_analysis/atom.py
@@ -1,18 +1,30 @@
 import itertools
 
 class Atom(object):
+    """Represents a single persistent atom during a simulation
+
+    Attributes:
+        species_strings (str): """
     
     newid = itertools.count(1)
     
-    def __init__(self, species):
-        self.atom_species = species
+    def __init__(self, species_string):
+        """Initialise an Atom object
+
+        Args:
+            species_string (str): String for this atom species, e.g. 'Li'.
+
+        Returns:
+            None
+        """
+        self.species_string = species_string
         self.index = next(Atom.newid)
         self.in_polyhedron = None
         self._frac_coords = None
         
     def get_coords(self, structure):
         atom_species_sites = [ s for s in structure 
-                                if s.species_string is self.atom_species ]
+                                if s.species_string is self.species_string ]
         self._frac_coords = atom_species_sites[self.index-1].frac_coords
         
     @property

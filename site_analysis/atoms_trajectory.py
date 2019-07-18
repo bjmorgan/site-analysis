613bb2088fbe8bd415d4b4339b798ab86bd9b7b3
Benjamin Morgan <benmorgan2@gmail.com>
2019-07-18 10:49:56 +0100

    Started rewriting Trajectory classes

--- site_analysis/atoms_trajectory.py
+++ site_analysis/atoms_trajectory.py
@@ -2,37 +2,23 @@ import numpy as np
 
 class AtomsTrajectory(object):
     
-    def __init__(self, atoms=None, atom_lookup=None):
-        self.data = []
-        self.timesteps = []
-        if atoms:
-            self.atom_lookup = {a.index: i for i, a in enumerate(atoms)}
-        elif atom_lookup:
-            self.atom_lookup = atom_lookup
-        else:
-            raise ArgumentError('atoms or atom_lookup arguments are needed to initialise an AtomsTrajectory')
+    def __init__(self, atoms):
+        self.atoms = atoms
+        self.atom_lookup = {a.index: i for i, a in enumerate(atoms)}
         
-    def append_timestep(self, atom_sites, t=None):
-        self.data.append(atom_sites)
-        self.timesteps.append(t)
-
-    def by_atom_index(self, i):
-        return np.array(self.data)[:,self.atom_lookup[i]]
-
-    def reset(self):
-        self.data = []
-        self.timesteps = []
+    def by_atom_indices(self, indices):
+        return AtomsTrajectory( atoms=[a for a in self.atoms if a.index in indices] )
+   
+    @property
+    def data(self):
+        return list(map(list, zip(*[atom.trajectory for atom in self.atoms])))
 
     def as_dict(self):
-        d = {'data': self.data,
-             'timesteps': self.timesteps,
-             'atom_lookup': self.atom_lookup}
+        d = {'atoms': self.atoms}
         return d
 
     @classmethod
     def from_dict(cls, d):
-        at = cls( atom_lookup=d['atom_lookup'] )
-        at.data = d['data']
-        at.timesteps = d['timesteps']
+        at = cls(atoms=d['atoms'])
         return at
         
--- site_analysis/sites_trajectory.py
+++ site_analysis/sites_trajectory.py
@@ -1,36 +1,25 @@
 class SitesTrajectory(object):
     
-    def __init__(self, sites=None, site_lookup=None):
-        self.data = []
-        self.timesteps = []
-        if sites:
-            self.site_lookup = {s.index: i for i, s in enumerate(sites)}
-        elif site_lookup:
-            self.site_lookup = site_lookup
-        else:
-            raise ArgumentError('sites or site_lookup arguments are needed to initialise an SitesTrajectory') 
+    def __init__(self, sites):
+        self.sites = sites
+        self.site_lookup = {s.index: i for i, s in enumerate(sites)}
 
-    def append_timestep(self, site_occupations, t=None):
-        self.data.append(site_occupations)
-        self.timesteps.append(t)
+    def by_site_indices(self, indices):
+        return SitesTrajectory( sites=[s for s in self.sites if s.index in indices ] )
 
-    def by_site_index(self, i):
-        return [ d[self.site_lookup[i]] for d in self.data ]
+    def by_site_label(self, label):
+        return SitesTrajectory( sites=[s for s in self.sites is s.label is label] )
 
-    def reset(self):
-        self.data = []
-        self.timesteps = []
+    @property
+    def data(self):
+        return list(map(list, zip(*[site.trajectory for site in self.sites])))
+
 
     def as_dict(self):
-        d = {'data': self.data,
-             'timesteps': self.timesteps,
-             'site_lookup': self.site_lookup}
-
+        d = {'sites': self.sites}
         return d
 
     @classmethod
     def from_dict(cls, d):
-        st = cls( site_lookup=d['site_lookup'])
-        st.data = d['data']
-        st.timesteps = d['timesteps']
+        st = cls(sites=d['sites'])
         return st

8a40a42daad50c061bc660dc0206751a672becd2
Benjamin Morgan <Benjmain>
2019-10-25 16:57:19 +0100

    Removed imports from __init__

--- site_analysis/__init__.py
+++ site_analysis/__init__.py
@@ -1,10 +1,1 @@
-from .atom import Atom
-from .polyhedral_site import PolyhedralSite
-from .shortest_distance_site import ShortestDistanceSite
-from .spherical_site import SphericalSite
-from .analysis import Analysis
-from .atoms_trajectory import AtomsTrajectory
-from .sites_trajectory import SitesTrajectory
-from .tools import get_vertex_indices
-
 __version__ = '0.0.1'

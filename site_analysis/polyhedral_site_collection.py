367b16ea919fb4bf02e8a5ef9f4278e0d37af0ff
Benjamin Morgan <benmorgan2@gmail.com>
2019-11-21 18:47:43 +0000

    Added methods for finding neighbouring polyhedral sites

--- site_analysis/polyhedral_site_collection.py
+++ site_analysis/polyhedral_site_collection.py
@@ -2,6 +2,38 @@ from .site_collection import SiteCollection
 
 class PolyhedralSiteCollection(SiteCollection):
 
+    def __init__(self, sites):
+        super(PolyhedralSiteCollection, self).__init__(sites)
+        self._neighbouring_sites = self.construct_neighbouring_sites()
+
+    def construct_neighbouring_sites(self):
+        """
+        Find all polyhedral sites that are face-sharing neighbours.
+
+        Any polyhedral sites that share 3 or more vertices are considered
+        to share a face.
+
+        Args:
+            None
+
+        Returns:
+            (dict): Dictionary of `int`: `list` entries. 
+                Keys are site indices. Values are lists of PolyhedralSite objects.
+
+        """
+        neighbours = {}
+        for site_i in self.sites:
+            neighbours[site_i.index] = []
+            for site_j in self.sites:
+                if site_i is site_j:
+                    continue
+                # count the number of shared vertices.
+                n_shared_vertices = len( set(site_i.vertex_indices) & set(site_j.vertex_indices) ) 
+                # if this is >= 3 these sites share a face.
+                if n_shared_vertices >= 3:
+                    neighbours[site_i.index].append(site_j)
+        return neighbours
+
     def analyse_structure(self, atoms, structure):
         for a in atoms:
             a.get_coords(structure)
@@ -24,6 +56,10 @@ class PolyhedralSiteCollection(SiteCollection):
                 if s.contains_atom(atom):
                     self.update_occupation( s, atom )
                     break
+
+    def neighbouring_sites(self, index):
+        return self._neighbouring_sites[index] 
+
 
     
  

f2323c885c4a82ead09015b2bcb6297c88b24370
Benjamin Morgan <benmorgan2@gmail.com>
2019-11-23 10:33:13 +0000

    Added docstrings and tests for VoronoiSite class

--- site_analysis/voronoi_site.py
+++ site_analysis/voronoi_site.py
@@ -1,18 +1,53 @@
 from .site import Site
 
 class VoronoiSite(Site):
+    """Site subclass corresponding to Voronoi cells centered
+    on fixed positions.
+
+    Attributes:
+        frac_coords (np.array): Fractional coordinates of the site centre.
+
+    """
     
     def __init__(self, frac_coords, label=None):
+        """Create a ``VoronoiSite`` instance.
+        
+        Args:
+            frac_coords (np.array): Fractional coordinates of the site centre.
+            label (:str:, optional): Optional label for this site. Default is `None`.
+
+        Returns:
+            None
+
+        """
         super(VoronoiSite, self).__init__(label=label)
         self.frac_coords = frac_coords
 
     def as_dict(self):
+        """Json-serializable dict representation of this VoronoiSite.
+
+        Args:
+            None
+
+        Returns:
+            (dict)
+
+        """
         d = super(VoronoiSite, self).as_dict()
         d['frac_coords'] = self.frac_coords
         return d
 
     @classmethod
     def from_dict(cls, d):
+        """Create a VoronoiSite object from a dict representation.
+
+        Args:
+            d (dict): The dict representation of this Site.
+
+        Returns:
+            (VoronoiSite)
+
+        """
         voronoi_site = cls( frac_coords=d['frac_coords'] )
         voronoi_site.label = d.get('label')
         return voronoi_site 
---
+++ tests/test_voronoi_site.py
@@ -0,0 +1,38 @@
+import unittest
+from site_analysis.voronoi_site import VoronoiSite
+from unittest.mock import patch, MagicMock, Mock
+import numpy as np
+
+class VoronoiSiteTestCase(unittest.TestCase):
+
+    def test_voronoi_site_is_initialised(self):
+        frac_coords = np.array([0.1, 0.2, 0.3])
+        site = VoronoiSite(frac_coords=frac_coords)
+        np.testing.assert_array_equal(site.frac_coords, frac_coords)
+        self.assertEqual(site.label, None)
+
+    def test_voronoi_site_is_initialised_with_label(self):
+        frac_coords = np.array([0.1, 0.2, 0.3])
+        site = VoronoiSite(frac_coords=frac_coords, label='foo')
+        np.testing.assert_array_equal(site.frac_coords, frac_coords)
+        self.assertEqual(site.label, 'foo')
+
+    def test_as_dict(self):
+        frac_coords = np.array([0.1, 0.2, 0.3])
+        site = VoronoiSite(frac_coords=frac_coords, label='foo')
+        site_dict = site.as_dict()
+        np.testing.assert_array_equal(site_dict['frac_coords'], frac_coords)
+        self.assertEqual(site_dict['label'], 'foo')
+   
+    def test_from_dict(self):
+        site_dict = {'frac_coords': np.array([0.1, 0.2, 0.3]),
+                     'label': 'foo'}
+        with patch('site_analysis.site.Site.from_dict') as mock_from_dict:
+            mock_from_dict.return_value = {}
+            site = VoronoiSite.from_dict(site_dict)
+            self.assertEqual(site.label, 'foo')
+            np.testing.assert_array_equal(site.frac_coords, site_dict['frac_coords'])
+
+if __name__ == '__main__':
+    unittest.main()
+    

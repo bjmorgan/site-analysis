4691f137e7832b5de0f85114d8e209dd4b44c763
Benjamin <b.j.morgan@bath.ac.uk>
2022-10-16 08:38:10 +0100

    Fixed abstract class testing

--- tests/test_site.py
+++ tests/test_site.py
@@ -4,6 +4,18 @@ from site_analysis.atom import Atom
 from unittest.mock import patch, MagicMock, Mock
 import numpy as np
 from collections import Counter
+from typing import Any
+
+class ConcreteSite(Site):
+
+    def centre(self) -> np.ndarray:
+        raise NotImplementedError
+
+    def contains_point(self,
+                       x: np.ndarray,
+                       *args: Any,
+                       **kwargs: Any) -> bool:
+        raise NotImplementedError
 
 class SiteTestCase(unittest.TestCase):
 
@@ -11,7 +23,7 @@ class SiteTestCase(unittest.TestCase):
         Site._newid = 0
 
     def test_site_is_initialised(self):
-        site = Site()
+        site = ConcreteSite()
         self.assertEqual(site.index, 0)
         self.assertEqual(site.label, None)
         self.assertEqual(site.contains_atoms, [])
@@ -20,16 +32,16 @@ class SiteTestCase(unittest.TestCase):
         self.assertEqual(site.transitions, {})
 
     def test_site_is_initialised_with_label(self):
-        site = Site(label='foo')
+        site = ConcreteSite(label='foo')
         self.assertEqual(site.label, 'foo')
 
     def test_site_index_autoincrements(self):
-        site1 = Site()
-        site2 = Site()
+        site1 = ConcreteSite()
+        site2 = ConcreteSite()
         self.assertEqual(site2.index, site1.index + 1)
 
     def test_reset(self):
-        site = Site()
+        site = ConcreteSite()
         site.contains_atoms = ['foo']
         site.trajectory = ['bar']
         site.transitions = Counter([4])
@@ -39,17 +51,17 @@ class SiteTestCase(unittest.TestCase):
         self.assertEqual(site.transitions, {})
 
     def test_contains_point_raises_not_implemented_error(self):
-        site = Site()
+        site = ConcreteSite()
         with self.assertRaises(NotImplementedError):
             site.contains_point(np.array([0,0,0]))
 
     def test_centre_raises_not_implemented_error(self):
-        site = Site()
+        site = ConcreteSite()
         with self.assertRaises(NotImplementedError):
             site.centre()
 
     def test_contains_atom(self):
-        site = Site()
+        site = ConcreteSite()
         atom = Mock(spec=Atom)
         atom.frac_coords = np.array([0,0,0])
         site.contains_point = Mock(return_value=True)
@@ -58,7 +70,7 @@ class SiteTestCase(unittest.TestCase):
         self.assertFalse(site.contains_atom(atom))
 
     def test_as_dict(self):
-        site = Site()
+        site = ConcreteSite()
         site.index = 7
         site.contains_atoms = [3]
         site.trajectory = [10,11,12]
@@ -81,18 +93,18 @@ class SiteTestCase(unittest.TestCase):
 
     def test_reset_index(self):
         Site._newid = 7
-        site = Site()
+        site = ConcreteSite()
         self.assertEqual(site.index, 7)
         Site.reset_index()
-        site = Site()
+        site = ConcreteSite()
         self.assertEqual(site.index, 0)
         
     def test_reset_index_to_defined_index(self):
         Site._newid = 7
-        site = Site()
+        site = ConcreteSite()
         self.assertEqual(site.index, 7)
         Site.reset_index(newid=12)
-        site = Site()
+        site = ConcreteSite()
         self.assertEqual(site.index, 12)
    
     def test_from_dict(self):
@@ -102,7 +114,7 @@ class SiteTestCase(unittest.TestCase):
                      'points': [np.array([0,0,0])],
                      'label': 'foo',
                      'transitions': Counter([3,3,2])}
-        site = Site.from_dict(site_dict)
+        site = ConcreteSite.from_dict(site_dict)
         self.assertEqual(site.index, site_dict['index'])
         self.assertEqual(site.contains_atoms, site_dict['contains_atoms'])
         self.assertEqual(site.trajectory, site_dict['trajectory'])
